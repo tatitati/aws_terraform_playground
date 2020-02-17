7a7e9226395610ff28fb5a1be6a203f2c79c56c7
Francisco Albusac <francisco.albusac@bbc.co.uk>
2020-02-17 19:48:52 +0000

    Mon 17 Feb 2020 19:48:52 GMT

--- main.tf
+++ main.tf
@@ -3,6 +3,45 @@ provider "aws" {
 }
 
 
-module "sqs" {
-  source = "./modules/sqs"
+# module "sqs" {
+#   source = "./modules/sqs"
+# }
+
+# resource "aws_instance" "my-test-instance" {
+#   ami             = "ami-fa7cdd89"
+#   instance_type   = "t2.micro"
+#   tags = {
+#     Name = "test-instance"
+#   }
+# }
+
+resource "aws_cloudformation_stack" "network" {
+  name = "networking-stack"
+
+  parameters = {
+    VPCCidr = "10.0.0.0/16"
+  }
+
+  template_body = <<STACK
+{
+  "Parameters" : {
+    "VPCCidr" : {
+      "Type" : "String",
+      "Default" : "10.0.0.0/16",
+      "Description" : "Enter the CIDR block for the VPC. Default is 10.0.0.0/16."
+    }
+  },
+  "Resources" : {
+    "myVpc": {
+      "Type" : "AWS::EC2::VPC",
+      "Properties" : {
+        "CidrBlock" : { "Ref" : "VPCCidr" },
+        "Tags" : [
+          {"Key": "Name", "Value": "Primary_CF_VPC"}
+        ]
+      }
+    }
+  }
+}
+STACK
 }
