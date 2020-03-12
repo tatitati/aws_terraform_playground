82c947a356e825704c3cc1bb3057bb96910d31cf
Francisco Albusac <francisco.albusac@bbc.co.uk>
2020-03-12 23:25:20 +0000

    more

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,10 +1,10 @@
-# EC2 user data
+# user data
 # -------------
 data "template_file" "init" {
   template = "${file("${path.module}/my_user_data.sh")}"
 }
 
-# SG to ssh into the instance
+# SG to ssh
 # --------------------------
 resource "aws_security_group" "ssh_access_terraform" {
   name = "ec2_ssh_access__terraform"
@@ -16,11 +16,21 @@ resource "aws_security_group" "ssh_access_terraform" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # Allow the ec2 to communicate with the exterior
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
-
+# MAIN
+# --------------------------
 resource "aws_instance" "my-test-instance" {
-  ami             = "ami-07cda0db070313c52"
+  ami             = "ami-0ec1ba09723e5bfac"
   instance_type   = "t2.micro"
   key_name        = "new_ec2_keypar"
   user_data       = data.template_file.init.rendered
