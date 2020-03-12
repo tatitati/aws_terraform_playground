3d1217d8bd019b4cafa2ef281a534b9095f68bb3
Francisco Albusac <francisco.albusac@bbc.co.uk>
2020-03-12 23:45:03 +0000

    more

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,11 +1,11 @@
 # user data
-# -------------
+# ---------
 data "template_file" "init" {
   template = "${file("${path.module}/my_user_data.sh")}"
 }
 
-# SG to ssh
-# --------------------------
+# SG to ssh and can connect with exterior
+# --------------------------------------
 resource "aws_security_group" "ssh_access_terraform" {
   name = "ec2_ssh_access__terraform"
 
@@ -24,17 +24,67 @@ resource "aws_security_group" "ssh_access_terraform" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
+# Attaches a Managed IAM Policy to an IAM role to read S3
+#--------------------------------------------------------
+resource "aws_iam_role" "role" {
+  name                = "terraform-role"
+  assume_role_policy  = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy" {
+  name          = "test-policy"
+  description   = "A test policy"
+  policy        = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = aws_iam_role.role.name
 }
 
 # MAIN
-# --------------------------
+# ----
 resource "aws_instance" "my-test-instance" {
-  ami             = "ami-0ec1ba09723e5bfac"
-  instance_type   = "t2.micro"
-  key_name        = "new_ec2_keypar"
-  user_data       = data.template_file.init.rendered
-  security_groups = ["${aws_security_group.ssh_access_terraform.name}"]
+  ami                   = "ami-0ec1ba09723e5bfac"
+  instance_type         = "t2.micro"
+  key_name              = "new_ec2_keypar"
+  user_data             = data.template_file.init.rendered
+  security_groups       = ["${aws_security_group.ssh_access_terraform.name}"]
+  iam_instance_profile  = aws_iam_instance_profile.test_profile.name
 
   tags = {
     Name = "test-instance"
