2fbade18c29921a11182938ff3216d53c4e3521b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2020-03-20 14:39:14 +0000

    more

--- modules/ecs/ecs.tf
+++ modules/ecs/ecs.tf
@@ -1,4 +1,6 @@
 resource "aws_ecs_task_definition" "service" {
   family                = "service"
   container_definitions = "${file("modules/ecs/service.json")}"
+  memory                = 300
+  cpu                   = 250
 }
