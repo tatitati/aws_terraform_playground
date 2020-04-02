cba87777efe02ba91ef91700d81967966e6c3116
Francisco Albusac <francisco.albusac@bbc.co.uk>
2020-04-02 20:04:54 +0100

    Thu  2 Apr 2020 20:04:54 BST

--- main.tf
+++ main.tf
@@ -11,6 +11,16 @@ provider "aws" {
 #   source = "./modules/ec2"
 # }
 
-module "ecs" {
-  source = "./modules/ecs"
+# module "ecs" {
+#   source = "./modules/ecs"
+# }
+
+# Import this with
+# arn=arn:aws:kms:eu-central-1:3384.....:key/1c88223b......
+# terraform import aws_kms_key.example $arn
+resource "aws_kms_key" "example" {
+  description = "example"
+  deletion_window_in_days = 10
+
+  policy = "${file("modules/kms/kms-policy.json")}"
 }
